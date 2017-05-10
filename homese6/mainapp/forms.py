743e1ad3f7e9c38be7318cc31a29a70ce026bf36
psicho <9400878@gmail.com>
2017-05-10 17:22:40 +0400

    Доработана админка.

--- homese6/mainapp/forms.py
+++ homese6/mainapp/forms.py
@@ -9,7 +9,25 @@ class MyRegistrationForm(UserCreationForm):
     last_name = forms.CharField(label='Фамилия', required=False)
     class Meta:
         model = User
-        # fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
+        #fields = ('__all__')
+    def save(self, commit=True):
+        user = super(MyRegistrationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
+
+class MyCorrectionForm(UserCreationForm):
+    username = forms.CharField(label='Пользователь', required=True)
+    email = forms.EmailField(label='Почта', required=True)
+    first_name = forms.CharField(label='Имя', required=False)
+    last_name = forms.CharField(label='Фамилия', required=False)
+    class Meta:
+        model = User
+        #fields = ('username', 'email', 'password1', 'password2')
         fields = ('__all__')
     def save(self, commit=True):
         user = super(MyRegistrationForm, self).save(commit=False)
--- homese6/mainapp/views.py
+++ homese6/mainapp/views.py
@@ -94,3 +94,38 @@ def delete_user(request, user_id):
     user = get_object_or_404(User, id=user_id)
     user.delete()
     return HttpResponseRedirect('/admin007/')
+
+def create_user(request, user_id=None):
+    """
+    Создает Пользователя(User)
+    Или редактирует существующего, если указан  user_id
+    """
+    if request.is_ajax():
+        if not user_id:
+            user = User(request.POST)
+        else:
+            user = get_object_or_404(User, id=user_id)
+            user = MyRegistrationForm(request.POST or None, instance=user)
+        if user.is_valid():
+            user.save()
+            users = User.objects.all()
+            html = loader.render_to_string('users_list.html', {'users': users}, request=request)
+            data = {'errors': False, 'html': html}
+            return JsonResponse(data)
+        else:
+            errors = user.errors.as_json()
+            return JsonResponse({'errors': errors})
+    raise Http404
+def get_user_form(request, user_id):
+    """
+    Возвращает заполненную форму для редактирования Пользователя(User) с заданным user_id
+    """
+    if request.is_ajax():
+        user = get_object_or_404(User, id=user_id)
+        user_form = MyRegistrationForm(instance=user)
+        context = {'form': user_form, 'id': user_id}
+        context.update(csrf(request))
+        html = loader.render_to_string('inc-registration_form.html', context)
+        data = {'errors': False, 'html': html}
+        return JsonResponse(data)
+    raise Http404
